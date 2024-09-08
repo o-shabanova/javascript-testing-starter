f4696a6188a7aac72559e8f4d7c1d2c0614f96ec
Olga Shabanova <shabanova994@gmail.com>
2024-09-08 18:57:45 +0300

    Writing parametrized tests for isPriceInRange function

--- tests/core.test.js
+++ tests/core.test.js
@@ -146,3 +146,17 @@ describe('canDrive testing with parametrized tests', () => {
         expect(canDrive(age, country)).toBe(result);
     })
 });
+
+//Writing test for isPriceInRange function with parameterized tests
+
+describe('isPriceInRange', () => {
+    it.each([
+        {scenario: 'price < min', price: -10, result: false},
+        {scenario: 'price = min', price: 0, result: true},
+        {scenario: 'price between min and max', price: 50, result: true},
+        {scenario: 'price = max', price: 100, result: true},
+        {scenario: 'price > max', price: 200, result: false},
+    ])('should return $result when $scenario', ({ price, result}) => {
+        expect(isPriceInRange(price, 0, 100)).toBe(result);
+    })
+})
